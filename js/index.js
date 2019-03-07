68d3995287254a88e7840744e3ac1fdb92cc4108
Michael Chung <mchung96@stanford.edu>
2019-03-07 14:24:44 -0800

    fixed path to assignments.json

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 $().ready(function() {
 	console.log("youaklsjdflkasjdflasjdf");
-	$.getJSON("res/assignments.json", function(data) {
+	$.getJSON("../res/assignments.json", function(data) {
 		console.log("HELLO");
-		let assigns = data.assigns;
+		let assigns = data["assigns"];
 		for (let i = 0; i < assigns.length; i++) {
 			console.log(assigns[i].id);
 		}
