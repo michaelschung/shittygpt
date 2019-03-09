4a04d486700c33f5b486d2c58a4badf1d9ffc5fe
Michael Chung <mchung96@stanford.edu>
2019-03-08 23:58:15 -0800

    calendar with events

--- js/B5-calendar.js
+++ js/B5-calendar.js
@@ -2,28 +2,95 @@ console.log(Date());
 
 var grid = document.querySelector(".grid-container");
 
-var events = cal.events;
-var assigns = cal.assigns;
-var assessments = cal.assessments;
+setUpCalendar();
+loadEvents();
 
-for (var i = 0; i < daysInTerm; i++) {
-	var date = Util.nDaysLater(termStart, i);
-	var id = (date.getTime()/100000).toString();
+function loadEvents() {
+	var events = cal.events;
+	var assigns = cal.assigns;
+	var assessments = cal.assessments;
 	
-	grid.appendChild(Util.tag(
-		"div",
-		{
-			"class": "grid-item",
-			"id": id
-		},
-		date.getDate()
-	));
+	for (var i in events) {
+		var e = events[i];
+//		console.log(e);
+		var date = new Date(e.date).getTime();
+		var content = e.content.split(" // ");
+		var id = (date/100000).toString();
+		
+		for (var i in content) {
+			$("#"+id).children (".content").append(Util.tag("div", {}, content[i]));
+		}
+	}
+}
+
+function setUpCalendar() {
+	for (var i = 0; i < daysInTerm; i++) {
+		var day = Util.nDaysLater(termStart, i);
+		var id = (day.getTime()/100000).toString();
+
+		var date = Util.tag(
+			"div",
+			{"class": "date"},
+			day.getDate()
+	//		Util.tag("div", {}, day.getDate())
+		);
+
+		var content = Util.tag(
+			"div",
+			{"class": "content"},
+			""
+		);
+
+		var due = Util.tag(
+			"div",
+			{"class": "due"},
+			""
+		);
+
+		var dayTemplate = [date, content];
+		if (day.getDay() > 0 && day.getDay() < 6) {
+			dayTemplate.push(due);
+		}
+
+		grid.appendChild(Util.tag(
+			"div",
+			{
+				"class": "grid-item",
+				"id": id
+			},
+			dayTemplate
+		));
+	}
 }
 
 function adjustGridItemSize() {
-	var itemW = $(".grid-item").width();
+	var item = $(".grid-item");
+	var itemW = item.width();
+	
 	$(".grid-item").css({
-		"height": 1.5*itemW + "px"
+		"height": 1.3*itemW + "px"
+	});
+	
+	var itemH = item.height();
+	$(".date").css({
+		"width": itemW/4 + "px",
+		"height": itemH/6.6 + "px",
+		"font-size": itemH/8 + "px"
+	});
+	
+	$(".content").css({
+		"top": itemH/6 + "px",
+		"height": itemH/2.2 + "px",
+		"padding": itemW/15 + "px",
+		"font-size": itemH/12 + "px"
+	});
+	
+	$(".due").css({
+		"bottom": 0,
+		"height": itemH/4.5 + "px",
+		"padding": itemW/15 + "px",
+		"font-size": itemH/12 + "px",
+		"font-style": "italic"
 	});
 }
 
