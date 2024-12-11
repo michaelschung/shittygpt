2b24f0384e5c5fe5bce97c18817ea0b5083c45db
Michael Chung <michaelchung@michaels-air.mynetworksettings.com>
2024-12-11 14:12:37 -0500

    removed other

--- OTHER/clcs-website/class-materials/assignments/hw5-photo-generator/js/populate-favs.js
+++
@@ -1,8 +0,0 @@
-var favs = JSON.parse(localStorage.getItem("favs"));
-
-for (var i = 0; i < favs.length; i++) {
-	var newImg = document.createElement("img");
-	newImg.src = favs[i];
-	newImg.className = "fav";
-	document.body.appendChild(newImg);
-}
